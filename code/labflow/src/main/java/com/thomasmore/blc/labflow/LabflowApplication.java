cf7ab2e607359cbb04c59bb7782aeb01a2e55436
Nathan Neve <nathanneve@live.be>
2024-10-03 12:07:05 +0200

    aanpassing datamodel

--- code/labflow/src/main/java/com/thomasmore/blc/labflow/LabflowApplication.java
+++ code/labflow/src/main/java/com/thomasmore/blc/labflow/LabflowApplication.java
@@ -3,6 +3,7 @@ package com.thomasmore.blc.labflow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class LabflowApplication {
 
