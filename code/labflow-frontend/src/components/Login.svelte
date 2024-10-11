2effa7de05d8e200152e6d164f04741db0c64bc0
Nathan Neve <nathanneve@live.be>
2024-10-11 16:18:09 +0200

    tailwind config & login frontend

---
+++ code/labflow-frontend/src/components/Login.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Button from './Button.svelte'
+    import Input from './Input.svelte'
+</script>
+
+<form class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col w-full mt-10 md:mt-0">
+    <div class="flex justify-between">
+        <h1 class="text-gray-900 text-lg font-medium title-font mb-5">Log in met je gegevens</h1>
+    </div>
+    <Input label="Email" type="text" required value="" />
+    <Input label="Paswoord" type="password" required value=""/>
+    <Button>Aanmelden</Button>
+</form>
