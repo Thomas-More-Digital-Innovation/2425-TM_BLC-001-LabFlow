32c21a1cc039a19b95c437276b5c151cce07387b
Nathan Neve <nathanneve@live.be>
2024-10-14 16:28:57 +0200

    staal component

---
+++ code/labflow-frontend/src/components/Staal.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+
+</script>
+
+<p class="w-full bg-white rounded-lg h-12">STAAAAAAAAAAAL</p>
--- code/labflow-frontend/src/routes/stalen/+page.svelte
+++ code/labflow-frontend/src/routes/stalen/+page.svelte
@@ -5,6 +5,8 @@
     // @ts-ignore
     import IoMdSettings from 'svelte-icons/io/IoMdSettings.svelte'
     import { getRol } from '$lib/globalFunctions';
+	import Staal from "../../components/Staal.svelte";
+
 
     let bgColor = 'bg-blue-400';
     let pointerEvent = 'pointer-events-auto';
@@ -17,7 +19,7 @@
 </script>
 
 <Nav />
-<div class="px-8">
+<div class="px-8 flex flex-row space-x-5">
     <div class="flex flex-col space-y-5">
         <a href='/stalen/nieuw' class="bg-blue-400 flex flex-col items-center justify-center w-72 h-72 rounded-2xl">
             <div class="w-40 h-40 text-white flex items-center justify-center">
@@ -33,5 +35,12 @@
             </div>
             <p class="text-white text-2xl text-center mt-2">Instellingen</p>
         </a>
+    </div>
+    <div class="bg-slate-200 w-full h-full rounded-2xl p-5">
+        <form action="" class="flex space-x-5 mb-5">
+            <input type="text" id="search" name="search" placeholder="zoeken" class="w-full h-12 rounded-lg text-black pl-3">
+            <input type="date" id="date" name="date" class="w-full h-12 rounded-lg text-black px-3">
+        </form>
+        <Staal />
     </div>
 </div>
