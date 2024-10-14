9223147fc457d1e5bfe7b176b26a2cfee468f4a7
Nathan Neve <nathanneve@live.be>
2024-10-14 12:50:45 +0200

    uitlogfunctionaliteit
    uitloggen en knoppen stalen

---
+++ code/labflow-frontend/src/components/nav.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+    import logoLabflow from '$lib/assets/labflowLogo.svg';
+    // @ts-ignore
+    import GoPerson from 'svelte-icons/go/GoPerson.svelte';
+
+    function eraseCookie() {
+        // https://stackoverflow.com/questions/2144386/how-to-delete-a-cookie
+        document.cookie = "authToken=;expires=" + new Date(0).toUTCString()
+    }
+    
+    function logout() {
+        goto('/')
+    }
+</script>
+
+<div class="py-2 px-8 flex justify-between items-center">
+    <img src={logoLabflow} alt='logo Labflow & Thomas More' class="h-10">
+    <div class="w-28 h-10 flex justify-between items-center">
+        <div class="flex flex-col justify-between h-full">
+            <p class="text-sm">student</p>
+            <button class="text-xs underline" on:click={() => { logout(); eraseCookie(); }} type="button">
+                Uitloggen
+            </button>
+        </div>
+        <div class="w-8 h-8 p-1 bg-slate-400 rounded-full flex items-center justify-center mr-1">
+            <GoPerson/>
+        </div>
+    </div>
+</div>
+
--- code/labflow-frontend/src/routes/stalen/+page.svelte
+++ code/labflow-frontend/src/routes/stalen/+page.svelte
@@ -1,1 +1,25 @@
-<h1>Staalpagina</h1>
+<script>
+    import Nav from "../../components/nav.svelte";
+    // @ts-ignore
+    import GoPlus from 'svelte-icons/go/GoPlus.svelte';
+    // @ts-ignore
+    import IoMdSettings from 'svelte-icons/io/IoMdSettings.svelte'
+</script>
+
+<Nav />
+<div class="px-8 mt-4">
+    <div class="flex flex-col space-y-5">
+        <div class="bg-blue-400 flex flex-col items-center justify-center w-72 h-72 rounded-2xl">
+            <div class="w-40 h-40 text-white flex items-center justify-center">
+                <GoPlus />
+            </div>
+            <p class="text-white text-2xl text-center mt-2">Nieuwe staal</p>
+        </div>
+        <div class="bg-blue-400 flex flex-col items-center justify-center w-72 h-72 rounded-2xl">
+            <div class="w-40 h-40 text-white flex items-center justify-center">
+                <IoMdSettings />
+            </div>
+            <p class="text-white text-2xl text-center mt-2">Instellingen</p>
+        </div>
+    </div>
+</div>
