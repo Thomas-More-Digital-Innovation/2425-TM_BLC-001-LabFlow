2649ba44fda06446e8ebc7982df8077d013fa0c8
Nathan Neve <nathanneve@live.be>
2024-10-23 14:38:56 +0200

    opsmuk clicked setting pannel

--- code/labflow-frontend/src/routes/instellingen/+page.svelte
+++ code/labflow-frontend/src/routes/instellingen/+page.svelte
@@ -6,28 +6,35 @@
     import GiTestTubes from 'svelte-icons/gi/GiTestTubes.svelte';
     // @ts-ignore
     import FaUserAlt from 'svelte-icons/fa/FaUserAlt.svelte';
+    // @ts-ignore
+    import GiHypodermicTest from 'svelte-icons/gi/GiHypodermicTest.svelte'
     import CategorieënBeheren from '../../components/Instellingen/CategorieënBeheren.svelte';
     import GebruikersBeheren from '../../components/Instellingen/GebruikersBeheren.svelte';
 	import StalenBeheren from '../../components/Instellingen/StalenBeheren.svelte';
+	import TestsBeheren from '../../components/Instellingen/Testsbeheren.svelte';
 
     // default staat nu op testen beheren
-    let navElement = "Staal";
+    let navElement = "Gebruiker";
 </script>
 
 <Nav/>
 <div class="px-8 flex flex-row">
 
-    <!-- 3 knoppen -->
-    <div class="flex flex-col space-y-3 w-40 mt-[73px]">
-        <button type="button" on:click={() => navElement = "Categorie"} class="p-3 text-center text-lg bg-blue-400 flex flex-col items-center justify-center w-40 h-40 rounded-2xl text-white">
+    <!-- 4 knoppen -->
+    <div class="flex flex-col space-y-3 w-40 mt-[74px]">
+        <button type="button" on:click={() => navElement = "Categorie"} class="p-3 text-center text-lg {navElement == "Categorie" ? 'bg-blue-500' : 'bg-blue-400'} flex flex-col items-center justify-center w-40 h-40 rounded-2xl text-white">
             <div class="w-14 h-14 mb-5"><FaClipboardList /></div>
             Categorieën beheren
         </button>
-        <button type="button" on:click={() => navElement = "Staal"} class="p-3 text-center text-lg bg-blue-400 flex flex-col items-center justify-center w-40 h-40 rounded-2xl text-white">
+        <button type="button" on:click={() => navElement = "Staal"} class="p-3 text-center text-lg {navElement == "Staal" ? 'bg-blue-500' : 'bg-blue-400'} flex flex-col items-center justify-center w-40 h-40 rounded-2xl text-white">
             <div class="w-14 h-14 mb-5"><GiTestTubes /></div>
             Stalen beheren
         </button>
-        <button type="button" on:click={() => navElement = "Gebruiker"} class="p-3 text-center text-lg bg-blue-400 flex flex-col items-center justify-center w-40 h-40 rounded-2xl text-white">
+        <button type="button" on:click={() => navElement = "Test"} class="p-3 text-center text-lg {navElement == "Test" ? 'bg-blue-500' : 'bg-blue-400'} flex flex-col items-center justify-center w-40 h-40 rounded-2xl text-white">
+            <div class="w-14 h-14 mb-5"><GiHypodermicTest /></div>
+            Tests beheren
+        </button>
+        <button type="button" on:click={() => navElement = "Gebruiker"} class="p-3 text-center text-lg {navElement == "Gebruiker" ? 'bg-blue-500' : 'bg-blue-400'} flex flex-col items-center justify-center w-40 h-40 rounded-2xl text-white">
             <div class="w-14 h-14 mb-5"><FaUserAlt /></div>
             Gebruikers beheren
         </button>
@@ -40,5 +47,7 @@
         <StalenBeheren/>
     {:else if navElement === "Gebruiker"}
         <GebruikersBeheren/>
+    {:else if navElement === "Test"}
+    <TestsBeheren/>
     {/if}
 </div>
