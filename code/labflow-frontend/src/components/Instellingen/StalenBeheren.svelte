12ad75e563ac8b425d2b3fdf3786bca42d4532d0
Nathan Neve <nathanneve@live.be>
2024-10-23 13:42:10 +0200

    frontend CREATE

--- code/labflow-frontend/src/components/Instellingen/StalenBeheren.svelte
+++ code/labflow-frontend/src/components/Instellingen/StalenBeheren.svelte
@@ -6,6 +6,8 @@
     import GoX from 'svelte-icons/go/GoX.svelte';
     // @ts-ignore
     import FaTrashAlt from 'svelte-icons/fa/FaTrashAlt.svelte';
+    // @ts-ignore
+    import FaPlus from 'svelte-icons/fa/FaPlus.svelte'
     import { onMount } from 'svelte';
     import { fetchStalen } from "$lib/fetchFunctions";
     import { getCookie } from "$lib/globalFunctions";
@@ -55,10 +57,96 @@
         return;
     }
 
+    ///// POST Staal aanmaken /////
+    let StaalCode = '';
+    let patientAchternaam = '';
+    let patientVoornaam = '';
+    let patientGeslacht = '';
+    let patientGeboorteDatum = '';
+    let laborantNaam = '';
+    let laborantRnummer = '';
+
+    let errorVeldenStaalPOST = {
+        staalcode: false,
+        naam: false,
+        voornaam: false,
+        geslacht: false,
+        geboortedatum: false,
+        laborantNaam: false,
+        laborantRnummer: false    
+    }
+
+    async function nieuweStaal() {
+        return;
+    }
+
+
+
+
+    ///// PUT Staal aanpassen /////
+    let errorVeldenStaalPUT = {
+        staalcode: false,
+        naam: false,
+        voornaam: false,
+        geslacht: false,
+        geboortedatum: false,
+        laborantNaam: false,
+        laborantRnummer: false
+    }
+
+    let errorMessageStaal = '';
+
     async function updateStaal(id: string) {
         const staal = stalen.find(s => s.id === id);
         console.log(staal);
         if (!staal) return;
+
+        let isValid = true;
+        staal.laborantRnummer = staal.laborantRnummer.toUpperCase();
+        // regex voor R-nummer: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions/Character_classes 
+        const regex = /^R\d{7}$/;
+        const errorVeldenStaal = {
+            staalcode: false,
+            naam: false,
+            voornaam: false,
+            geslacht: false,
+            geboortedatum: false,
+            laborantNaam: false,
+            laborantRnummer: false
+        }
+        if (!staal.laborantNaam) {
+            errorVeldenStaalPUT.laborantNaam = true;
+        }
+        if (!staal.laborantRnummer || !regex.test(staal.laborantRnummer)) {
+            errorVeldenStaalPUT.laborantRnummer = true;
+        }
+        if (staal.laborantNaam && staal.laborantRnummer && regex.test(staal.laborantRnummer)) {
+            isValid = true;
+        }
+        if (!staal.patientAchternaam) {
+            errorVeldenStaalPUT.naam = true;
+            isValid = false;
+        }
+        if (!staal.patientVoornaam) {
+            errorVeldenStaalPUT.voornaam = true;
+            isValid = false;
+        }
+        if (!staal.patientGeboorteDatum) {
+            errorVeldenStaalPUT.geboortedatum = true;
+            isValid = false;
+        }
+        if (!staal.patientGeslacht) {
+            errorVeldenStaalPUT.geslacht = true;
+            isValid = false;
+        }
+        if (!staal.StaalCode) {
+            errorVeldenStaalPUT.staalcode = true;
+            isValid = false;
+        }
+        if (!isValid) {
+            errorMessageStaal = 'Vul alle verplichte velden in.';
+            return;
+        }
         try {
             await fetch(`http://localhost:8080/api/updatestaal/${id}`, {
                 method: "PUT",
@@ -80,6 +168,7 @@
                     }
                 }),
             });
+            errorMessageStaal = '';
         } catch (error) {
             console.error("Staal kon niet worden aangepast: ", error);
         }
@@ -109,7 +198,7 @@
         </div>
         <div class="space-y-3">
             <!-- Header -->
-            <div class="grid grid-cols-8 bg-gray-300 rounded-lg h-10 items-center px-3 font-bold space-x-3">
+            <div class="grid grid-cols-9 bg-gray-300 rounded-lg h-10 items-center px-3 font-bold space-x-3">
                 <div class="col-span-1 text-left">
                     <p>Aanmaakdatum</p>
                 </div>
@@ -129,16 +218,62 @@
                     <p>Geboortedatum</p>
                 </div>
                 <div class="col-span-1 text-left ">
-                    <p>Laborant</p>
+                    <p>Naam Laborant</p>
+                </div>
+                <div class="col-span-1 text-left ">
+                    <p>Rnummer Laborant</p>
                 </div>
                 <div class="col-span-1 text-right">
                     <p>Acties</p>
                 </div>
             </div>
+            <div class="grid grid-cols-9 space-x-3 bg-white rounded-lg h-20 items-center px-3 shadow-md">
+                <div class="col-span-1">
+                    <p class="bg-white rounded-lg h-14 text-lg pl-3 w-full" />
+                </div>
+                <div class="col-span-1">
+                    <input type="text" id="StaalCode" bind:value={StaalCode} class="bg-gray-100 rounded-lg h-14 text-lg pl-3 w-full  
+                    {errorVeldenStaalPOST.staalcode ? 'border-2 border-red-500' : ''}" />
+                </div>
+                <div class="col-span-1">
+                    <input type="text" id="patientAchternaam" bind:value={patientAchternaam} class="bg-gray-100 rounded-lg h-14 text-lg pl-3 w-full  
+                    {errorVeldenStaalPOST.naam ? 'border-2 border-red-500' : ''}" />
+                </div>
+                <div class="col-span-1">
+                    <input type="text" id="patientVoornaam" bind:value={patientVoornaam} class="bg-gray-100 rounded-lg h-14 text-lg pl-3 w-full  
+                    {errorVeldenStaalPOST.voornaam ? 'border-2 border-red-500' : ''}" />
+                </div>
+                <div class="col-span-1">
+                    <input type="text" id="patientGeslacht" bind:value={patientGeslacht} class="bg-gray-100 rounded-lg h-14 text-lg pl-3 w-full  
+                    {errorVeldenStaalPOST.geslacht ? 'border-2 border-red-500' : ''}" />
+                </div>
+                <div class="col-span-1">
+                    <input type="text" id="patientGeboorteDatum" bind:value={patientGeboorteDatum} class="bg-gray-100 rounded-lg h-14 text-lg pl-3 w-full  
+                    {errorVeldenStaalPOST.geboortedatum ? 'border-2 border-red-500' : ''}" />
+                </div>
+                <div class="col-span-1">
+                    <input type="text" id="laborantNaam" bind:value={laborantNaam} class="bg-gray-100 rounded-lg h-14 text-lg pl-3 w-full  
+                    {errorVeldenStaalPOST.laborantNaam ? 'border-2 border-red-500' : ''}" />
+                </div>
+                <div class="col-span-1">
+                    <input type="text" id="laborantRnummer" bind:value={laborantRnummer} class="bg-gray-100 rounded-lg h-14 text-lg pl-3 w-full  
+                    {errorVeldenStaalPOST.laborantRnummer ? 'border-2 border-red-500' : ''}" />
+                </div>
 
+                <!-- Acties -->
+                <div class="col-span-1 flex justify-end">
+                    <button type="button" class="h-10 w-10 bg-green-500 p-2 rounded-lg text-white" 
+                    on:click={nieuweStaal} aria-label="Nieuwe categorie toevoegen">
+                        <FaPlus />
+                    </button>
+                </div>
+            </div>
+            {#if errorMessageStaal}
+            <div class="text-red-500 mb-2">{errorMessageStaal}</div>
+            {/if}
             <div class="space-y-3">
                 {#each filteredStalen as staal, index}
-                    <div class="grid grid-cols-8 bg-white rounded-lg h-20 items-center px-3 shadow-md space-x-3">
+                    <div class="grid grid-cols-9 bg-white rounded-lg h-20 items-center px-3 shadow-md space-x-3">
 
                         <div class="col-span-1">
                             <span 
@@ -180,10 +315,16 @@
                                 bind:value={staal.patientGeboorteDatum}
                                 class="bg-gray-100 rounded-lg h-14 text-lg pl-3 w-full p-2" />
                         </div>
-                        <div class="col-span-1 ">
+                        <div class="col-span-1">
                             <input type="text" 
                                 on:blur={() => updateStaal(staal.id)} 
                                 bind:value={staal.laborantNaam} 
+                                class="bg-gray-100 rounded-lg h-14 text-lg pl-3 w-full" />
+                        </div>
+                        <div class="col-span-1">
+                            <input type="text" 
+                                on:blur={() => updateStaal(staal.id)} 
+                                bind:value={staal.laborantRnummer} 
                                 class="bg-gray-100 rounded-lg h-14 text-lg pl-3 w-full" />
                         </div>
             
