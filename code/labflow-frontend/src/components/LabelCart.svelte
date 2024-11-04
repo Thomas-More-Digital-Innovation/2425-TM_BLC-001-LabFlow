56b21881680d4ba0ca6acb4ed0c96ffd116c1f87
NETbrew <113974723+cesarvanleuffelen@users.noreply.github.com>
2024-11-04 13:11:43 +0100

    color fix

--- code/labflow-frontend/src/components/LabelCart.svelte
+++ code/labflow-frontend/src/components/LabelCart.svelte
@@ -6,5 +6,5 @@
 
 <div class=" border border-gray-200 rounded-xl flex justify-between items-center p-4 my-3">
 	<p class="font-bold text-lg">{labelName}</p>
-	<p class={`p-6 rounded-full text-white bg-[${color}]`}></p>
+	<p class={`p-6 rounded-full text-white`} style={`background-color: ${color};`}></p>
 </div>
