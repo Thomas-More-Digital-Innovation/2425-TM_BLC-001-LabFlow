4bd116fd36c2275b454f7445e00cef80d748e5cc
Nathan Neve <nathanneve@live.be>
2024-11-20 09:56:22 +0100

    backend

--- code/labflow-backend/src/main/java/com/thomasmore/blc/labflow/service/PdfGeneratorService.java
+++ code/labflow-backend/src/main/java/com/thomasmore/blc/labflow/service/PdfGeneratorService.java
@@ -97,8 +97,11 @@ public class PdfGeneratorService {
         LocalDate geboortedatum = staal.getPatientGeboorteDatum();
         char geslacht = staal.getPatientGeslacht();
 
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-        String formattedDate = formatter.format(geboortedatum);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formattedDate = geboortedatum.format(formatter);
+        // huidige datum
+        String formattedCurrentDate = LocalDate.now().format(formatter);
+
 
         // Create table for header section
         PdfPTable headerTable = new PdfPTable(2);
@@ -119,7 +122,7 @@ public class PdfGeneratorService {
         PdfPCell rightCell = new PdfPCell();
         rightCell.setBorder(Rectangle.NO_BORDER);
         rightCell.addElement(new Paragraph("Testcode: " + staalCode, bodyFont));
-        rightCell.addElement(new Paragraph("Datum: " + formatter.format(new Date()), bodyFont));  // Current date
+        rightCell.addElement(new Paragraph("Datum: " + formattedCurrentDate, bodyFont));  // Current date
         headerTable.addCell(rightCell);
 
         document.add(headerTable);
