3d43562bcd7256c5d78b4d0b9711bebc1a5ec3a1
Nathan Neve <nathanneve@live.be>
2024-11-21 16:38:38 +0100

    config change

--- code/labflow-frontend/svelte.config.js
+++ code/labflow-frontend/svelte.config.js
@@ -1,24 +1,24 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-// Update the config for PostCSS
 const config = {
-  prerender: {
-    handleHttpError: ({ path, referrer, message }) => {
-      // ignore deliberate link to shiny 404 page
-      // https://svelte.dev/docs/kit/configuration#prerender
-      if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
-        return;
-      }
-    },
-  },
   preprocess: vitePreprocess({
-    postcss: true // Enables PostCSS processing, which is required for TailwindCSS
+    postcss: true // Enables PostCSS processing, required for TailwindCSS
   }),
-  
 
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+
+    prerender: {
+      handleHttpError: ({ path, referrer, message }) => {
+        // Ignore deliberate link to shiny 404 page
+        if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+          return;
+        }
+      },
+      // https://svelte.dev/docs/kit/configuration#prerender
+      handleMissingId: 'ignore', // Suppress errors for missing fragment identifiers, dit geeft een cloudflare error
+    },
   }
 };
 
