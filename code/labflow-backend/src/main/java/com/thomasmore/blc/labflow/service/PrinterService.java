6b3bb9b948f787afb3c4facad6bfb1d18d69f04f
NETbrew <113974723+cesarvanleuffelen@users.noreply.github.com>
2024-11-22 14:36:37 +0100

    added zpl code and start to extra function

--- code/labflow-backend/src/main/java/com/thomasmore/blc/labflow/service/PrinterService.java
+++ code/labflow-backend/src/main/java/com/thomasmore/blc/labflow/service/PrinterService.java
@@ -1,11 +1,15 @@
 package com.thomasmore.blc.labflow.service;
 
 import com.thomasmore.blc.labflow.entity.PrintRequest;
+import com.thomasmore.blc.labflow.repository.StaalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PrinterService {
+    @Autowired
+    private StaalRepository staalRepository;
 
     public ResponseEntity<String> printLabel(PrintRequest request) {
         try {
@@ -14,11 +18,44 @@ public class PrinterService {
             }
 
             // generate ZPL code
-            String zplCode = "Hello world!";
+            String zplCode = "^XA\n" +
+                    "^PW450\n" +
+                    "^LL250\n" +
+                    "^FO10,10^GB430,230,3^FS\n" +
+                    "^FO20,20^A0N,30,30^FD" + "César" + "^FS\n" +
+                    "^FO200,20^A0N,30,30^FD" + "Van Leuffelen" + "^FS\n" +
+                    "^FO20,60^A0N,25,25^FD" + "Geboorte: 29 juli 2004" + "^FS\n" +
+                    "^FO20,100^A0N,25,25^FD" + "Geslacht: Man" + "^FS\n" +
+                    "^FO170,200^A0N,25,25^FD" + "2024000001" + "^FS\n" +
+                    "^FO390,60^A0N,30,30^FR^FWR^FD" + "Heparine" + "^FS\n" +
+                    "^FO360,100^A0N,20,20^FWR^FD" + "groen" + "^FS\n" +
+                    "^XZ";
 
             return ResponseEntity.ok(zplCode);
         } catch (Exception e) {
             return ResponseEntity.internalServerError().body("Error while printing label");
         }
     }
+
+    /*private String generateZplCode(int labelCount) {
+        // base label ZPL code
+        String standardLabel = "^XA\n" +
+                "^PW450\n" +
+                "^LL250\n" +
+                "^FO10,10^GB430,230,3^FS\n" +
+                "^FO20,20^A0N,30,30^FD" + "César" + "^FS\n" +
+                "^FO200,20^A0N,30,30^FD" + "Van Leuffelen" + "^FS\n" +
+                "^FO20,60^A0N,25,25^FD" + "Geboorte: 29 juli 2004" + "^FS\n" +
+                "^FO20,100^A0N,25,25^FD" + "Geslacht: Man" + "^FS\n" +
+                "^FO170,200^A0N,25,25^FD" + "2024000001" + "^FS\n" +
+                "^FO390,60^A0N,30,30^FR^FWR^FD" + "Heparine" + "^FS\n" +
+                "^FO360,100^A0N,20,20^FWR^FD" + "groen" + "^FS\n" +
+                "^XZ";
+
+        // generate ZPL per category label
+
+        // create ZPL code total
+
+        // return all ZPL codes
+    }*/
 }
