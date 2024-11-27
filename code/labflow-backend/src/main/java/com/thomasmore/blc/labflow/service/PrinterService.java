0fe6d3463f6044ad919ea059ed65444008faeacd
NETbrew <113974723+cesarvanleuffelen@users.noreply.github.com>
2024-11-27 11:21:16 +0100

    verwijderd extra commented code

--- code/labflow-backend/src/main/java/com/thomasmore/blc/labflow/service/PrinterService.java
+++ code/labflow-backend/src/main/java/com/thomasmore/blc/labflow/service/PrinterService.java
@@ -87,26 +87,4 @@ public class PrinterService {
             return ResponseEntity.internalServerError().body("Error while printing label");
         }
     }
-
-    /*private String generateZplCode(int labelCount) {
-        // base label ZPL code
-        String standardLabel = "^XA\n" +
-                "^PW450\n" +
-                "^LL250\n" +
-                "^FO10,10^GB430,230,3^FS\n" +
-                "^FO20,20^A0N,30,30^FD" + "César" + "^FS\n" +
-                "^FO200,20^A0N,30,30^FD" + "Van Leuffelen" + "^FS\n" +
-                "^FO20,60^A0N,25,25^FD" + "Geboorte: 29 juli 2004" + "^FS\n" +
-                "^FO20,100^A0N,25,25^FD" + "Geslacht: Man" + "^FS\n" +
-                "^FO170,200^A0N,25,25^FD" + "2024000001" + "^FS\n" +
-                "^FO390,60^A0N,30,30^FR^FWR^FD" + "Heparine" + "^FS\n" +
-                "^FO360,100^A0N,20,20^FWR^FD" + "groen" + "^FS\n" +
-                "^XZ";
-
-        // generate ZPL per category label
-
-        // create ZPL code total
-
-        // return all ZPL codes
-    }*/
 }
